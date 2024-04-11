19e343b981f051e4128c8d8c989841ac44283f53
priyampinju <pinjupriyam1826@gmail.com>
2024-04-12 00:28:48 +0530

    sds

--- src/App.jsx
+++ src/App.jsx
@@ -40,37 +40,99 @@ const App = () => {
 
   // }, []);
 
-  const timeline = gsap.timeline({
-    repeat: -1,
-    repeatDelay: 1,
-    yoyo: true,
-  });
+  // // // GSAP timeline
+
+  // const timeline = gsap.timeline({
+  //   repeat: -1,
+  //   repeatDelay: 1,
+  //   yoyo: true,
+  // });
+
+  // useGSAP(() => {
+  //   timeline.to("#blue-box", {
+  //     x: 500,
+  //     rotation: 360,
+  //     borderRadius: "100%",
+  //     duration: 1,
+  //     ease: "back.inOut",
+  //   });
+
+  //   // // lets put third gsap animation
+  //   timeline.to("#blue-box", {
+  //     y: -100,
+  //     scale: 1,
+  //     borderRadius: 0,
+  //     rotation: 360,
+  //     duration: 2,
+  //     ease: "back.inOut",
+  //   });
+
+  //   timeline.to("#blue-box", {
+  //     y: 100,
+  //     scale: 1,
+  //     borderRadius: "100%",
+  //     rotation: 360,
+  //     duration: 2,
+  //     ease: "back.inOut",
+  //   });
+
+  //   timeline.to("#blue-box", {
+  //     x: 1000,
+  //     scale: 1,
+  //     rotation: 720,
+  //     borderRadius: 0,
+  //     duration: 1,
+  //     ease: "back.inOut",
+  //   });
+  // });
+
+  // // // gsap Stagger
 
   useGSAP(() => {
-    timeline.to("#blue-box", {
-      x: 500,
+    gsap.to(".stagger-box", {
+      y: 250,
       rotation: 360,
       borderRadius: "100%",
-      duration: 1,
-      ease: "back.inOut",
+      repeat: -1,
+      yoyo: true,
+      stagger: 0.05,
+      ease: "circ.inOut",
     });
-
-    timeline.to("#blue-box", {
-      x: 1000,
-      scale: 1,
-      rotation: 720,
-      borderRadius: 0,
-      duration: 1,
-      ease: "back.inOut",
-    });
-  });
+  }, []);
 
   return (
     <main>
       <h1>GsapTo</h1>
 
-      <div className="mt-20 rounded-3xl w-[1150px] p-5 bg-slate-500">
+      <div className="mt-10 rounded-3xl w-[1150px] p-5 bg-slate-500">
         <div id="blue-box" className="w-20 h-20 bg-blue-500 " />
+      </div>
+      <div className="mt-10 ml-5 pl-3">
+        <button
+          onClick={() => {
+            if (timeline.paused()) {
+              timeline.play();
+            } else {
+              timeline.pause();
+            }
+          }}
+          className="border-2 pr-2 pl-2 pt-1 pb-1"
+        >
+          Play/Pause
+        </button>
+      </div>
+
+      <div className="mt-10">
+        <div className="flex gap-5">
+          <div className="w-20 h-20 bg-indigo-200 rounded-lg stagger-box"></div>
+          <div className="w-20 h-20 bg-indigo-300 rounded-lg stagger-box"></div>
+          <div className="w-20 h-20 bg-indigo-400 rounded-lg stagger-box"></div>
+          <div className="w-20 h-20 bg-indigo-500 rounded-lg stagger-box"></div>
+          <div className="w-20 h-20 bg-indigo-600 rounded-lg stagger-box"></div>
+          <div className="w-20 h-20 bg-indigo-700 rounded-lg stagger-box"></div>
+          <div className="w-20 h-20 bg-indigo-800 rounded-lg stagger-box"></div>
+          <div className="w-20 h-20 bg-indigo-900 rounded-lg stagger-box"></div>
+        </div>
       </div>
     </main>
   );
