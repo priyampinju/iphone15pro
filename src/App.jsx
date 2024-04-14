3e3f361e1d7c59562eb01bcf89935381257f271d
priyampinju <pinjupriyam1826@gmail.com>
2024-04-14 16:12:48 +0530

    sdfd

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Highlights from "./components/Highlights";
 import Hero from "./components/Hero";
+import Model from "./components/Model";
 
 const App = () => {
   return (
     <main className="bg-black">
       <Navbar />
       <Hero />
-
       <Highlights />
+      <Model />
     </main>
   );
 };
