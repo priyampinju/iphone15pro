eb5407d6d0c8566033d4f173085d67411bd6fea3
priyampinju <pinjupriyam1826@gmail.com>
2024-04-16 16:22:27 +0530

    model done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Highlights from "./components/Highlights";
 import Hero from "./components/Hero";
 import Model from "./components/Model";
+import Features from "./components/Features";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Hero />
       <Highlights />
       <Model />
+      <Features />
     </main>
   );
 };
---
+++ src/components/Features.jsx
@@ -0,0 +1,49 @@
+import { useGSAP } from "@gsap/react";
+import { animateWithGsap } from "../utils/animations";
+import { exploreVideo } from "../utils";
+
+const Features = () => {
+  useGSAP(() => {
+    animateWithGsap("#features_title", {
+      y: 0,
+      opacity: 1,
+    });
+  }, []);
+  return (
+    <section className="h-full common-padding bg-zinc overflow-hidden relative">
+      <div className="screen-max-width">
+        <div className="mb-12 w-full">
+          <h1 className="section-heading" id="features_title">
+            Explore the full story
+          </h1>
+        </div>
+
+        <div className="flex flex-col justify-center items-center overflow-hidden">
+          <div className="mt-32 mb-24 pl-24">
+            <h2 className="text-5xl lg:text-7xl font-semibold">iPhone</h2>
+            <h2 className="text-5xl lg:text-7xl font-semibold">
+              Forged in titanium
+            </h2>
+          </div>
+
+          <div className="flex-cen flex-col sm:px-10">
+            <div className="relative h-[50vh] w-full flex items-center">
+              <video
+                playsInline
+                id="exploreVideo"
+                className="w-full h-full object-cover object-center"
+                preload="none"
+                muted
+                autoPlay
+              >
+                <source src={exploreVideo} type="video/mp4" />
+              </video>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Features;
