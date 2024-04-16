addfa725b7718dffd6a5e8118b848b6bad89883f
priyampinju <pinjupriyam1826@gmail.com>
2024-04-16 22:45:47 +0530

    model done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Highlights from "./components/Highlights";
 import Hero from "./components/Hero";
 import Model from "./components/Model";
 import Features from "./components/Features";
+import HowItWorks from "./components/HowItWorks";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <Highlights />
       <Model />
       <Features />
+      <HowItWorks />
     </main>
   );
 };
--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,9 +1,24 @@
 import { useGSAP } from "@gsap/react";
 import { animateWithGsap } from "../utils/animations";
 import { explore1Img, explore2Img, exploreVideo } from "../utils";
+import gsap from "gsap/gsap-core";
+import { useRef } from "react";
 
 const Features = () => {
+  const videoRef = useRef();
+
   useGSAP(() => {
+    gsap.to("#exploreVideo", {
+      scrollTrigger: {
+        trigger: "#exploreVideo",
+        toggleActions: "play pause reverse restart",
+        start: "-10% bottom",
+      },
+      onComplete: () => {
+        videoRef.current.play();
+      },
+    });
+
     animateWithGsap("#features_title", {
       y: 0,
       opacity: 1,
@@ -14,6 +29,12 @@ const Features = () => {
       { scale: 1, opacity: 1, ease: "power1" },
       { scrub: 10 }
     );
+
+    animateWithGsap(".g_text", {
+      y: 0,
+      opacity: 1,
+      ease: "power2InOut",
+    });
   }, []);
   return (
     <section className="h-full common-padding bg-zinc overflow-hidden relative">
@@ -61,6 +82,29 @@ const Features = () => {
                     alt="titanium"
                     className="feature-video g_grow"
                   />
+                </div>
+              </div>
+              <div className="feature-text-container">
+                <div className="flex-1 flex-center">
+                  <p className="feature-text g_text">
+                    iPhone 15 pro is {``}
+                    <span className="text-white">
+                      the first iPhone to feature an aerospace grade titanium
+                      design {""}
+                    </span>
+                    using the same alloy that spacecrafts use for missions to
+                    Mars.
+                  </p>
+                </div>
+                <div className="flex-1 flex-center">
+                  <p className="feature-text g_text">
+                    Titanium has one of the best strength-to-weight ratios of
+                    any metal, making these our {``}
+                    <span className="text-white">
+                      lightest pro models ever. {""}
+                    </span>
+                    You'll notice the difference the moment you pick one up.
+                  </p>
                 </div>
               </div>
             </div>
