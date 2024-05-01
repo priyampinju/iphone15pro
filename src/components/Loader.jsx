23d5128765f7ab7a72281d5d8a9ac2e8c7b70be3
priyampinju <pinjupriyam1826@gmail.com>
2024-05-02 02:10:40 +0530

    update

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -5,7 +5,9 @@ const Loader = () => {
   return (
     <Html>
       <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
-        <div className="w-[10vw] h-[10vw] rounded-full">Loading...</div>
+        <div className="w-[10vw] h-[10vw] rounded-full">
+          3D model is loading...
+        </div>
       </div>
     </Html>
   );
--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -5,6 +5,7 @@ import Lights from "./Lights";
 import Loader from "./Loader";
 import Iphone from "./Iphone";
 import { Suspense } from "react";
+import svgmodel from "../../public/assets/images/IPhone_15_Pro_Vector.svg";
 
 const ModelView = ({
   index,
@@ -16,48 +17,49 @@ const ModelView = ({
   size,
 }) => {
   return (
-    // <div className="modelview flex justify-center items-center h-full">
-    //   Your browser does not support 3D models.
-    // </div>
-
-    <View
-      index={index}
-      id={gsapType}
-      className={` border-2 border-red-500 w-full h-full absolute ${
-        index === 2 ? "right-[-100%]" : ""
-      }`}
-    >
-      {/* /* Ambient Light */}
-      <ambientLight intensity={0.3} />
-
-      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
-
-      <Lights />
-
-      <OrbitControls
-        makeDefault
-        ref={controlRef}
-        enableZoom={false}
-        enablePan={false}
-        rotateSpeed={0.4}
-        target={new THREE.Vector3(0, 0, 0)}
-        onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
-      />
-
-      <group
-        ref={groupRef}
-        name={`${index === 1} ? 'small' : 'large`}
-        position={[0, 0, 0]}
+    <>
+      <div className="model-container h-full flex justify-center w-full items-center">
+        <img src={svgmodel} alt="iPhone" width={240} className="opacity-20" />
+        <div className="modelview flex justify-center items-center h-full absolute">
+          Your browser does not support 3D models.{" "}
+        </div>
+      </div>
+      {/* 
+      <View
+        index={index}
+        id={gsapType}
+        className={` border-2 border-red-500 w-full h-full absolute ${
+          index === 2 ? "right-[-100%]" : ""
+        }`}
       >
-        <Suspense fallback={<Loader />}>
-          <Iphone
-            scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
-            item={item}
-            size={size}
-          />
-        </Suspense>
-      </group>
-    </View>
+        {/* /* Ambient Light */}
+      {/* <ambientLight intensity={0.3} /> */}
+      {/* <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+        <Lights />
+        <OrbitControls
+          makeDefault
+          ref={controlRef}
+          enableZoom={false}
+          enablePan={false}
+          rotateSpeed={0.4}
+          target={new THREE.Vector3(0, 0, 0)}
+          onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+        />
+        <group
+          ref={groupRef}
+          name={`${index === 1} ? 'small' : 'large`}
+          position={[0, 0, 0]}
+        >
+          <Suspense fallback={<Loader />}>
+            <Iphone
+              scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+              item={item}
+              size={size}
+            />
+          </Suspense>
+        </group> */}
+      {/* </View>  */}
+    </>
   );
 };
 
